b7b72b77321bd0ba47f0da90d47f05bf501f69ca
Crew Spence <csspence123@gmail.com>
2020-08-01 06:01:32 -0500

    Wrote working solution.

--- insertion_sort_udemy.js
+++ insertion_sort_udemy.js
@@ -4,5 +4,14 @@ Insertion Sort
 */
 
 const insertionSort = (arr) => {
-  
+  for(let i = 1; i < arr.length; i++) {
+    for(let j = i - 1; j > -1; j--) {
+      if(arr[j] > arr[j + 1]) {
+        let temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp; 
+      }
+    }
+  }
+  return arr;
 }
